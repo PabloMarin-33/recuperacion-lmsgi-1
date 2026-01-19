586f7cdb2492e4d1c2b1d9071cb7c96dd9aa8d19
Palomo-33 <pablomarinortega37@gmail.com>
2026-01-19 11:04:56 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const displayBee = document.querySelector("#bee")
+const displayBlank = document.querySelector("#blank")
+
+displayBee.textContent = "Este texto ha sido modificado por javascript"
+
+const bienvenida = ["Hola mundo", "Hello world", "Bonjour le monde", "Hallo Welt", "Ciao mondo", "Olá mundo"]
+
+function elegir(){
+    const rng = Math.floor(Math.random() * bienvenida.length)
+    const eleccionCiudad = bienvenida[rng]
+
+    displayBlank.textContent = eleccionCiudad
+}
